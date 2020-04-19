2a1ec3b2a116f2c3af407e4c856eedb325ff49dd
Weronika Żak <weronikarzak@gmail.com>
2020-04-19 20:34:37 +0200

    display spawned

--- PythonAPI/examples/t2.py
+++ PythonAPI/examples/t2.py
@@ -20,64 +20,104 @@ import random
 import time
 import cv2
 import numpy as np
+import math
 
 
-im_width, im_height = 640, 480
+SHOW_PREV = False
+IM_WIDTH, IM_HEIGHT = 640, 480
 
 
-def process_img(image):
-    i = np.array(image.raw_data)
-    i2 = i.reshape((im_height, im_width, 4))
-    i3 = i2[:, :, :3]
-    cv2.imshow("", i3)
-    cv2.waitKey(1)
-    return i3/255.0
+class CarEnvironment:
+    SHOW_CAM = SHOW_PREV
+    STEER_ATM = 1.0
+    actor_list = []
+    front_camera = None
+    collision_hist = []
 
-actor_list = []
 
-try:
-    client = carla.Client('localhost', 2000)
-    client.set_timeout(5.0)
+    def __init__(self):
 
-    world = client.get_world()
+        try:
+            self.client = carla.Client('localhost', 2000)
+            self.client.set_timeout(5.0)
 
-    blueprint_library = world.get_blueprint_library()
-    bp = blueprint_library.filter('model3')[0]
-    print(bp)
+            self.world = self.client.get_world()
 
-    spawn_point = random.choice(world.get_map().get_spawn_points())
+            blueprint_library = self.world.get_blueprint_library()
+            self.model_3 = blueprint_library.filter('model3')[0]
 
-    vehicle = world.spawn_actor(bp, spawn_point)
-    vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
-    # vehicle.set_autopilot(True)
 
-    actor_list.append(vehicle)
+        def reset(self):
+            self.collision_hist = []
+            self.actor_list = []
 
-    # sleep for 5 sec, then finish:
-    # time.sleep(5)
 
-    blueprint = blueprint_library.find('sensor.camera.rgb')
-    # changing dimensions
+            self.transform = random.choice(self.world.get_map().get_spawn_points())
 
-    blueprint.set_attribute('image_size_x', str(im_width))
-    blueprint.set_attribute('image_size_y', str(im_height))
-    blueprint.set_attribute('fov', '110')
+            self.vehicle = world.spawn_actor(self.model_3, self.transform)
 
-    # adjust sensor and attach to vehicle
-    spawn_point = carla.Transform(carla.Location(x=2.5, z=0.7))
+            self.actor_list.append(self.vehicle)
 
-    # spawn sensor and attach to vehicle
-    sensor = world.spawn_actor(blueprint, spawn_point, attach_to=vehicle)
+            self.rgb_cam = self.world.blueprint_library.find('sensor.camera.rgb')
 
-    # add sensor to list of actors
-    actor_list.append(sensor)
+            self.rgb_cam.set_attribute('image_size_x', str(IM_WIDTH))
+            self.rgb_cam.set_attribute('image_size_y', str(IM_HEIGHT))
+            self.rgb_cam.set_attribute('fov', '110')
 
-    sensor.listen(lambda data: process_img(data))
+            transform = carla.Transform(carla.Location(x=2.5, z=0.7))
 
-    time.sleep(5)
+            self.sensor = self.world.spawn_actor(self.rgb_cam, transform, attach_to=self.vehicle)
 
-finally:
-    print('destroying actors')
-    for actor in actor_list:
-        actor.destroy()
-    print('done.')
+            self.actor_list.append(self.sensor)
+            self.sensor.listen(lambda data: self.process_img(data))
+
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
+
+            time.sleep(4)
+
+
+        def process_img(self, image):
+            i = np.array(image.raw_data)
+            i2 = i.reshape((IM_HEIGHT, IM_WIDTH, 4))
+            i3 = i2[:, :, :3]
+            cv2.imshow("", i3)
+            cv2.waitKey(1)
+            return i3/255.0
+
+
+        def collision_data(self, event):
+            self.collision_hist.append(event)
+        
+
+        def step(self, action):
+
+            # 0 - left
+            # 1 - center
+            # 2 - right
+
+            if action == 0:
+                self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0))
+            if action == 1:
+                self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=-1*self.STEER_AMT))
+            if action == 2:
+                self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=1*self.STEER_AMT))
+
+            v = self.vehicle.get_velocity()
+            kmh = int(3.6 * math.sqrt(v.x**2, v.y**2 + v.z**2))
+
+            if len(self.collision_hist) != 0:
+                done = True
+                reward = -200
+            elif kmh < 50:
+                done = False
+                reward = -1
+            else:
+                done = False
+                reward = 1
+
+            # SECONDS_PER_EPISODE = 1000
+            
+            if self.episode_start + SEC_PER_EPISODE < time.time():
+                done = True
+
+            return self.front_camera, reward, done, None
